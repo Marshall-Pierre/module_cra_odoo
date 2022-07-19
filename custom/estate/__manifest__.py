1d9e2dbdde830b5c6ea8a421677862dd847e3084
NGUEMA ZENG Marshall-Pierre <nguemazengmp@gmail.com>
2022-07-19 12:15:10 +0100

    chap 6

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -3,10 +3,9 @@
     'depends': [
         'base',
     ],
-    'application': True,
     'data': [
         'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
         'views/estate_menus.xml',
+        'views/estate_property_views.xml',
     ]
 }
--- custom/estate/models/estate_property.py
+++ custom/estate/models/estate_property.py
@@ -1,3 +1,6 @@
+from soupsieve import select
+from tomlkit import value
+from traitlets import default
 from odoo import fields, models
 
 class Property(models.Model):
@@ -7,10 +10,10 @@ class Property(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Char()
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Datetime.today("09/02/2022"))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -18,5 +21,12 @@ class Property(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         string='Type',
-        selection=('Nord', 'Sud', 'Est', 'Ouest'),
+        selection=[('nord', 'Nord'), ('sud', 'Sud'), ('est', 'Est'), ('ouest', 'Ouest')],
+    )
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        string = 'State',
+        default = 'nouveau',
+        selection = [('nouveau', 'Nouveau'), ('offre reçue', 'Offre reçue'), ('offre acceptée', 'Offre acceptée'), ('vendue', 'Vendue'), ('annulée', 'Annulée')],
+        required = True,
     )
