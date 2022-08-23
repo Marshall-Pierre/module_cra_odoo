3218cb651e895bc1f554d1c246acb26d616304f8
Marshall-Pierre NGUEMA ZENG <nguemazengmp@gmail.com>
2022-08-23 10:55:01 +0100

    ajout du model project

--- b2mtechnologies/module_cra/models/project.py
+++ b2mtechnologies/module_cra/models/project.py
@@ -1,7 +1,10 @@
+from typing_extensions import Required
 from odoo import fields, models
 
 class ProjectModel(models.Model):
     _name = "project.model"
     _description = "Liste des projets"
 
+    project_manager_id = fields.Many2one("res.users", string="Chargé de project", default=lambda self: self.env.user, readonly=True)
     name = fields.Char(required=True)
+    project_implementer_id = fields.Many2many("res.users", string="Exécuatnt du projet")
